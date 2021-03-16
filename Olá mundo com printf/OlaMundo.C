8c912e0a6262ee167c0aa467ef7cefb1df06f8d5
Erllan Rego <erllanrego@hotmail.com>
2021-03-16 05:07:46 -0300

    update

---
+++ Olá mundo com printf/OlaMundo.C
@@ -0,0 +1,11 @@
+#include <stdio.h> /*Inclusão de bibliotecas, lugar onde estão guardadas funções prontas para podermos usarmos (comandos de entrada e saída)*/
+
+int main() { /*Um programa em C obrigatóriamente deve ter o "int main" como assinatura*/
+	
+	/*Tudo que estiver entre as chaves será o programa*/
+	
+	printf("Ola mundo!\n");/*TODO COMANDO TERMINA COM PONTO E VIRGULA*/
+	
+	return 0;/* Coloca-se esse comando antes de fechar a chave*/
+	
+}
