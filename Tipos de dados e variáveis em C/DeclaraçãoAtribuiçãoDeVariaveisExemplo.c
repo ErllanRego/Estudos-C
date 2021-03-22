919fa9bc878e0ce894e67019b7d6b030381a9dc2
Erllan Rego <erllanrego@hotmail.com>
2021-03-21 22:30:41 -0300

    Update DeclaraçãoAtribuiçãoDeVariaveisExemplo.c

--- Tipos de dados e variáveis em C/DeclaraçãoAtribuiçãoDeVariaveisExemplo.c
+++ Tipos de dados e variáveis em C/DeclaraçãoAtribuiçãoDeVariaveisExemplo.c
@@ -1,3 +1,28 @@
+/*#include <stdio.h>
+#include <string.h>
+int main() {
+int idade;
+double salario, altura;
+char genero;
+char nome[50];
+	idade = 20;
+	salario = 5800.5;
+	altura = 1.63;
+	genero = 'F';
+	strcpy(nome, "Maria Silva");
+		printf("IDADE = %d\n", idade);
+		printf("SALARIO = %.2lf\n", salario);
+		printf("ALTURA = %.2lf\n", altura);
+		printf("GENERO = %c\n", genero);
+		printf("NOME = %s\n", nome);
+		return 0;
+}
+*/
+
+
+
+
+
 #include <stdio.h>/*Vai permitir comandos de entrada e saída*/
 #include <string.h>/*Garante que funções de string funcionem*/
 
@@ -11,16 +36,16 @@ int main(){
 	char genero;/*Texto de um caractere*/
 	char nome[50];/*(VETOR DE CHAR)Texto com vários caracteres permitindo até 50 caracteres*/
 	
-	idade= 20;
-	salario= 5800.5;
-	altura= 1.63;
-	genero= 'F'; /*Lembrando que se usa aspas simples por ser apenas um caractere*/
+		idade = 20;
+		salario = 5800.5;
+		altura = 1.63;
+		genero = 'F'; /*Lembrando que se usa aspas simples por ser apenas um caractere*/
 	
-	/*PODE ATRIBUIR VALORES NA DECLARAÇÃO DE VARIAVEL TAMBÉM. EXEMPLO:
-	
-	int idade = 20;
-	char genero = 'F';
-	*/
+				/*PODE ATRIBUIR VALORES NA DECLARAÇÃO DE VARIAVEL TAMBÉM. EXEMPLO:
+				
+				int idade = 20;
+				char genero = 'F';
+				*/
 	
 	/*Por ser um vetor de char, para atribuir valor fica um pouco diferente
 	STRCPY É UMA FUNÇÃO PARA SE TRABALHAR COM TEXTO*/
@@ -36,14 +61,15 @@ int main(){
 	
 	printf("IDADE = %d\n", idade);/* %d indica para printf que um inteiro deve ser colocado na tela, logo depois é dado o valor deste inteiro.*/
 								  /*\n é meramente para mudar de linha*/
-	printf("SALARIO = %lf");
-	printf("ALTURA = %lf");
-	printf("GENERO = %c");
+	printf("SALARIO = %lf\n");
+	printf("ALTURA = %lf\n");
+	printf("GENERO = %c\n");
 	
 	
 	
-	
-	
-	
-	return 0;
+return 0;
 }
+
+
+
+
