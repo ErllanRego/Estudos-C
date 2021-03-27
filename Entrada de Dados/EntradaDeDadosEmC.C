6cd7ddd432d617d32c9a94c5ae09ed8faca56ac0
Erllan Rego <erllanrego@hotmail.com>
2021-03-27 16:18:02 -0300

    update

---
+++ Entrada de Dados/EntradaDeDadosEmC.C
@@ -0,0 +1,42 @@
+#include <stdio.h>//Permite entrada e saída
+#include <string.h>//Permite trabalhar com strings
+
+//Ação para evitar bug na hora de pegar os dados
+void limpar_entrada() {
+char c;
+while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+//COMANDO PARA QUE A QUEBRA DE LINHA NÃO SEJA ARMAZENADA
+void ler_texto(char *buffer, int length) {
+fgets(buffer, length, stdin);
+strtok(buffer, "\n");
+}
+	int main()
+	{
+		double salario1, salario2;
+		char nome1[50], nome2[50];
+		int idade;
+		char sexo;
+		printf("Nome da primeira pessoa: ");
+		ler_texto(nome1, 50);
+		printf("Salario da primeira pessoa: ");
+		scanf("%lf", &salario1);//Precisa ter o & porque interage com a memória
+		printf("Nome da segunda pessoa: ");
+		limpar_entrada();
+		ler_texto(nome2, 50);
+		printf("Salario da segunda pessoa: ");
+		scanf("%lf", &salario2);
+		printf("Digite uma idade: ");
+		scanf("%d", &idade);
+		printf("Digite um sexo (F/M): ");
+		limpar_entrada();
+		scanf("%c", &sexo);
+		printf("Nome 1: %s\n", nome1);
+		printf("Salario 1: %.2lf\n", salario1);
+		printf("Nome 2: %s\n", nome2);
+		printf("Salario 2: %.2lf\n", salario2);
+		printf("Idade: %d\n", idade);
+		printf("Sexo: %c\n", sexo);
+	return 0;
+	}
